902749816bcef6152db66e09c82c89c884caaaf0
jiaicon@163.com <jiaicon@163.com>
2019-07-02 16:32:00 +0800

    修改打包目录

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -3,7 +3,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     mode: 'production',
-    entry: './src/banner/banner.js',
+    entry: './src/banner/antd.js',
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, '../'),
