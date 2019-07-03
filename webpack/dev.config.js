c471557f2681731f5163a1533ba81428224a2d64
jiaicon@163.com <jiaicon@163.com>
2019-07-03 18:58:14 +0800

    测试包

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -13,7 +13,6 @@ module.exports = {
             {
                 test: /\.js$/,
                 use: 'babel-loader',
-                exclude: /node_modules/
             },
             {
                 test: /\.css$/,
