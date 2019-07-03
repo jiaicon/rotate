006ed0f3ebf750d79f49f5f6ced3a9798298be02
jiaicon@163.com <jiaicon@163.com>
2019-07-03 14:52:15 +0800

    上传index.js

---
+++ src/banner/index.js
@@ -0,0 +1,34 @@
+const _banner = require("./antd");
+const _uploadPhoto = require("./upload");
+const _rotate = require("./rotate");
+
+Object.defineProperty(exports, "Rotate", {
+    enumerable: true,
+    get: function get() {
+        return _rotate["default"];
+    }
+});
+
+Object.defineProperty(exports, "Banner", {
+    enumerable: true,
+    get: function get() {
+        return _banner["default"];
+    }
+});
+
+Object.defineProperty(exports, "UploadPhoto", {
+    enumerable: true,
+    get: function get() {
+        return _uploadPhoto["default"];
+    }
+});
+
+
+
+
+
+// module.exports = {
+//     Banner: _banner['default'],
+//     UploadPhoto: _uploadPhoto['default'],
+//     Rotate: _rotate['default']
+// };
