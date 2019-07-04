17d67817fc36e87e6973e7bcba22c29e8a7654f9
jiaicon@163.com <jiaicon@163.com>
2019-07-04 15:05:39 +0800

    修改banner

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Rotate} from './banner/index';
+import {Rotate, Banner} from './banner/index';
 
-let visible=false;
 class Test extends React.Component {
     state={
         visible: false
     };
     render() {
         return <div>
-            <Rotate visible={this.state.visible} src="https://tpc.googlesyndication.com/daca_images/simgad/15515071172727742888"/>
-            <div onClick={()=>this.setState({visible: true})}>点击</div>
+            {/*<Rotate visible={this.state.visible} src="https://tpc.googlesyndication.com/daca_images/simgad/15515071172727742888"/>*/}
+            {/*<div onClick={()=>this.setState({visible: true})}>点击</div>*/}
+            <Banner/>
         </div>
     }
 }
